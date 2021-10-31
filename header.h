63f7f4b571c278b4769b0bdf3d11f69f2108a472
CiscoPangPang <CiscoPangPang@gmail.com>
2021-10-31 02:14:08 -0700

    Make ip and tcp header

--- header.h
+++ header.h
@@ -1,17 +1,47 @@
 #include <stdint.h>
 
+typedef struct _ip_fragment {
+    uint16_t f_off:13;
+    uint8_t reserved_bit:1;
+    uint8_t no_fragment_bit:1;
+    uint8_t more_fragment_bit:1;
+} ip_fragment;
+
 typedef struct _ip_header {
-    uint8_t ver:4;
     uint8_t h_len:4;
+    uint8_t ver:4;
     uint8_t tos;
     uint16_t total_len;
     uint16_t id;
-    uint8_t flags:3;
-    uint16_t f_off:16;
+    ip_fragment frag;
     uint8_t ttl;
     uint8_t protocol;
     uint16_t checksum;
     uint32_t sip;
     uint32_t dip;
-    uint8_t pay[0x800];
 } ip_header;
+
+typedef struct _tcp_flags {
+    uint8_t fin:1;  
+    uint8_t syn:1;
+    uint8_t rst:1;
+    uint8_t psh:1;
+    uint8_t ack:1;
+    uint8_t urg:1;
+    uint8_t ece:1;
+    uint8_t cwr:1;
+    uint8_t ns:1; 
+    uint8_t reserved:3; 
+    uint8_t offset:4; //header lengtn / 4 
+} tcp_flags;
+
+typedef struct _tcp_header {
+    uint16_t sport;
+    uint16_t dport;
+    uint32_t seq_num;
+    uint32_t ack_num;
+    tcp_flags flags; 
+    uint16_t window;
+    uint16_t checksum;
+    uint16_t urgent_ptr;
+} tcp_header;
