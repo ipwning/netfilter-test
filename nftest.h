46e91f432b4526d4e76406ecb741ad42e389107f
CiscoPangPang <CiscoPangPang@gmail.com>
2021-10-31 02:16:52 -0700

    Frame implementation complete.

--- nftest.h
+++ nftest.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
 #include <linux/types.h>
@@ -8,9 +9,13 @@
 
 #include <libnetfilter_queue/libnetfilter_queue.h>
 
+#define TCP 6
+#define IP_STR "%hhu.%hhu.%hhu.%hhu"
+#define IP_ARG(ip) ip[0], ip[1], ip[2], ip[3]
+
 u_int32_t print_pkt (struct nfq_data *tb);
 
-void dump(unsigned char* buf, int size);
+void dump(char* buf, int size);
 
 int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
 	      struct nfq_data *nfa, void *data);
